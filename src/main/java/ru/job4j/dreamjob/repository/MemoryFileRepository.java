d025d9cb0c848eefafea4ebb0727a523efe555f2
Artyrio226 <artur_sar_80@mail.ru>
2023-12-23 13:57:06 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryFileRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryFileRepository.java
@@ -27,7 +27,7 @@ public class MemoryFileRepository implements FileRepository {
     }
 
     @Override
-    public void deleteById(int id) {
-        files.remove(id);
+    public boolean deleteById(int id) {
+        return files.remove(id) != null;
     }
 }
